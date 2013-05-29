2974fdea2cb9a4641dc0a68a6caaf92ad44c0967
gyang <gyang2007@foxmail.com>
2013-05-29 23:42:50 +0800

    初始化BerkeleyDB学习项目

---
+++ app.learn/src/main/java/com/gyang/test/recursion/HanoiTower.java
@@ -0,0 +1,15 @@
+package com.gyang.test.recursion;
+
+public class HanoiTower {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+	}
+	
+	public static void move(){
+		
+	}
+}
